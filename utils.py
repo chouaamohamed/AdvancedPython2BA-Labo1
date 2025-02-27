e5f40a6427264e8417832541e9751b5b03af9e85
Chouaa Mohamed <23105@ecam.be>
2025-02-27 10:54:39 +0100

    Test OK

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+import scipy.integrate
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +9,13 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	n2 = n
+
+	while n > 1:
+		n2 = n2*(n-1)
+		n = n-1
+	return n2
+		
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,7 +24,19 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b**2 - 4*a*c
+
+	if delta > 0:
+		root1 = (- b + math.sqrt(delta))/(2*a)
+		root2 = (- b - math.sqrt(delta))/(2*a)
+		return (root1, root2)
+
+	if delta < 0:
+		return ()
+	
+	if delta == 0:
+		root = (- b)/(2*a)
+		return (root)
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -28,8 +50,14 @@ def integrate(function, lower, upper):
 	Hint: You can use the 'integrate' function of the module 'scipy' and
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
-	"""
-	pass
+	"""	
+	def f(x):
+		return eval(function, {"x": x, "np": np})
+	
+	result, error = scipy.integrate.quad(f, lower, upper)
+
+	return result
+	
 
 if __name__ == '__main__':
 	print(fact(5))
